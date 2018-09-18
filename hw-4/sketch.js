fce53cd11c69064b567fa29c93c5b716ca26f35f
Scotty Graham <theboxcrab@gmail.com>
2018-09-18 11:29:29 -0600

    added color

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -1,12 +1,14 @@
 function setup()
 {
   createCanvas(720,480);
+  background('black')
 }
 function draw()
 {
   push();
   translate(360,240);
   rotate(-30);
+  fill('rgb(111, 63, 13)')
   rect(-100,-50,100,50);
   pop();
 
