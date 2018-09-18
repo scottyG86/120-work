1e1e55645cfc4d03a666cc84f73f946d818bf9f6
Scotty Graham <theboxcrab@gmail.com>
2018-09-18 12:13:58 -0600

    stuff

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -2,11 +2,11 @@ function setup()
 {
   createCanvas(720,480);
   background('black')
-  translate(360,240);
 }
 function draw()
 {
   push();
+  translate(360,240);
   rotate(-30);
   fill('rgb(13,111,52)')
   rect(-100,-50,100,50);
