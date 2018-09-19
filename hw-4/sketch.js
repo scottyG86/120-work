953a772a1b27ac03617d7e9b1bb3e45c7a367ff5
Scotty Graham <theboxcrab@gmail.com>
2018-09-18 22:47:17 -0600

    added moar legs

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -5,25 +5,43 @@ function setup()
 }
 function draw()
 {
-  //my character is going to resemble a demon spider
+  //my character is going to resemble a demon spider with 4 legs
   //sets background color
   background('black');
   push();//start character
   // centers character
     translate(360,240);
-    push();//body start
+
+    push();//2 front legs both sides
+      strokeWeight(20);
+      fill('rgb(58,102,14)');
+      //isolated so that I can rotate legs and see both sets
+        push();//left
+          rotate(30)
+          line(50,0,150,200);
+          line(150,200,150,270);
+        pop();
+
+        push();//right
+          rotate(-30)
+          line(-50,0,-150,-200);
+          line(-150,-200,-150,-270);
+        pop();
+    pop();//end front legs
+
+    push();//body
     //body code
       fill('rgb(73,115,32)');
       ellipse(-150,50,300,200);
     pop();//body end
 
-    push();//2 back legs
-    strokeWeight(20);
-    fill('rgb(58,102,14)');
-    line(0,0,150,200);
-    line(150,200,200,230);
-    line(-50,0,-150,-200);
-    line(-150,-200,-200,-230);
-    pop();
+    push();//2 back legs both sides
+      strokeWeight(20);
+      fill('rgb(38,66,9)');
+      line(0,0,150,200);//left
+      line(150,200,200,230);
+      line(-50,0,-150,-200);//right
+      line(-150,-200,-200,-230);
+    pop();//end back legs
   pop();//end character
 }
