bdbbe65d50d94c1b3a5755f91259063585441faf
Scotty Graham <theboxcrab@gmail.com>
2018-09-18 22:49:49 -0600

    moved thing so they looked better

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -12,6 +12,21 @@ function draw()
   // centers character
     translate(360,240);
 
+    push();//2 back legs both sides
+      strokeWeight(20);
+      fill('rgb(38,66,9)');
+      line(0,0,150,200);//left
+      line(150,200,200,230);
+      line(-50,0,-150,-200);//right
+      line(-150,-200,-200,-230);
+    pop();//end back legs
+
+    push();//body
+    //body code
+      fill('rgb(73,115,32)');
+      ellipse(-150,50,300,200);
+    pop();//body end
+
     push();//2 front legs both sides
       strokeWeight(20);
       fill('rgb(58,102,14)');
@@ -29,19 +44,5 @@ function draw()
         pop();
     pop();//end front legs
 
-    push();//body
-    //body code
-      fill('rgb(73,115,32)');
-      ellipse(-150,50,300,200);
-    pop();//body end
-
-    push();//2 back legs both sides
-      strokeWeight(20);
-      fill('rgb(38,66,9)');
-      line(0,0,150,200);//left
-      line(150,200,200,230);
-      line(-50,0,-150,-200);//right
-      line(-150,-200,-200,-230);
-    pop();//end back legs
   pop();//end character
 }
