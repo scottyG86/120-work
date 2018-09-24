ba1ca087b0aba06dc2af0893c26c55a549908d29
Scotty Graham <theboxcrab@gmail.com>
2018-09-24 13:32:49 -0600

    did great things

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -8,42 +8,11 @@ function draw()
   background('green');
   translate(360,240);
 
-  let red = 0;
-  let green = 0;
-  let blue = 0;
   let posX = 0;
   let posY = 0;
 
   fill('rgb 256,0,256');
   ellipse( posX, posY, 5, 5);
 
-  if(red < 256)
-  {
-    red = red + 1;
-  }
-  else if (red > 256)
-  {
-    red = red - 1;
-  }
-
-  if(green < 256)
-  {
-    green = green + 3;
-  }
-  else if (green > 256)
-  {
-    green = green - 3;
-  }
-
-  if(blue < 256)
-  {
-    blue = blue + 5;
-  }
-  else if (blue > 256)
-  {
-    blue = blue - 5;
-  }
-  text("r" + floor(red));
-  text("g" + floor(green));
-  text("b" + floor(blue));
+  posX = (posY * 3)+.7;
 }
