ff0e83803f08a59c08f959b6bb245699bbbf4c0c
Scotty Graham <theboxcrab@gmail.com>
2018-09-24 10:19:48 -0600

    added stuff

---
+++ hw-5/sketch.js
@@ -0,0 +1,17 @@
+setup()
+{
+  createCanvas(720,480);
+  background('black');
+}
+
+draw()
+{
+  let red = 0;
+  let green = 0;
+  let blue = 0;
+  let posX = 0;
+  let posY = 0;
+
+  fill('rgb(red,green,blue)');
+  ellipse( posX, posY, 40, 40);
+}
