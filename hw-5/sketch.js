558675f452996abc0d82c02e81b8d3bfc430d6ff
Scotty Graham <theboxcrab@gmail.com>
2018-09-26 15:27:51 -0600

    stuff

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -16,21 +16,23 @@ function draw()
   text( "frame rate: " + fr, 10, 20 );
   text( "posY " + posY, 10, 40 );
   text( "rot " + rot, 10, 60  );
-  translate(360,posY);
+  push()
+    translate(360,posY);
 
 
 
 
 
-  fill('rgb(200,50,0)');
-  ellipse(0 , 0, 1);
+    fill('rgb(200,50,0)');
+    ellipse(0 , 0, 1);
 
-  posY = posY + 1
+    posY = posY + 1
 
-  if(posY > 100 + 240)
-  {
-    posY = 240;
-    rotate(radians(rot));
-    rot= rot + rot
-  }
+    if(posY > 100 + 240)
+    {
+      posY = 240;
+      rotate(radians(rot));
+      rot= rot + rot
+    }
+  pop()
 }
