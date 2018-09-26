c223f7525b1f83b50078259fb5b855aec7afb702
Scotty Graham <theboxcrab@gmail.com>
2018-09-26 09:57:44 -0600

    did things

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,26 +1,28 @@
+let rot = 0;
+let posY = 0;
+
 function setup()
 {
-  createCanvas(720,480);
+createCanvas(720,480);
 
 }
 
 function draw()
 {
-  let fr = frameRate();
-  text( "frame rate: " + fr, 10, 20 );
-  push();
-  translate(width*0.5,height*0.5);
+let fr = frameRate();
+text( "frame rate: " + fr, 10, 20 );
+text( "posY " + posY, 10, 40 );
+translate(width*0.5,height*0.5);
 
-  background('green');
+background('green');
 
 
-  let rot = 0;
-  let posY = 0;
 
-  stroke('rgb 200,50,0');
-  strokeWeight(50)
-  point(0 , posY);
 
-  posY = posY + 1
-  pop();
+stroke('rgb 200,50,0');
+strokeWeight(50)
+point(0 , posY);
+
+posY = posY + 1
+pop();
 }
