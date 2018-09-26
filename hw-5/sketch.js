816c6763c752226de6bc074f941d346781c82617
Scotty Graham <theboxcrab@gmail.com>
2018-09-26 13:06:53 -0600

    stuff

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -11,17 +11,24 @@ background('green');
 function draw()
 {
 
-let fr = frameRate();
-text( "frame rate: " + fr, 10, 20 );
-text( "posY " + posY, 10, 40 );
-translate(width*0.5,height*0.5);
+
+  let fr = frameRate();
+  text( "frame rate: " + fr, 10, 20 );
+  text( "posY " + posY, 10, 40 );
+  translate(0,posY);
 
 
 
 
 
-stroke('rgb 200,50,0');
-ellipse(0 , posY, 1);
+  stroke('rgb 200,50,0');
+  ellipse(0 , 0, 1);
 
-posY = posY + 1
+  posY = posY + 1
+
+  if(posY > 100)
+  {
+    posY = 0;
+
+  }
 }
