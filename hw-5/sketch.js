aef1c305260db93adc45052813ac014847053b84
Scotty Graham <theboxcrab@gmail.com>
2018-09-26 15:52:09 -0600

    stuffds

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -13,13 +13,13 @@ function draw()
 
 
   let fr = frameRate();
-  push()
+  push();
     background('green');
     text( "frame rate: " + fr, 10, 20 );
     text( "posY " + posY, 10, 40 );
     text( "rot " + rot, 10, 60  );
-  pop()
-  push()
+  pop();
+  push();
     translate(360,posY);
 
 
@@ -29,13 +29,13 @@ function draw()
     fill('rgb(200,50,0)');
     ellipse(0 , 0, 1);
 
-    posY = posY + 1
+    posY = posY + 1;
 
     if(posY > 100 + 240)
     {
       posY = 240;
       rotate(radians(rot));
-      rot= rot + rot
+      rot= rot + 1;
     }
-  pop()
+  pop();
 }
