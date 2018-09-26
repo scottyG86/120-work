3995ed28906f31d535631363d665006320a4a542
Scotty Graham <theboxcrab@gmail.com>
2018-09-26 09:29:35 -0600

    aded frame rate

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,19 +1,26 @@
 function setup()
 {
   createCanvas(720,480);
-  translate(360,240);
+
 }
 
 function draw()
 {
+  let fr = frameRate();
+  text( "frame rate: " + fr, 10, 20 );
+  push();
+  translate(360,240);
+
   background('green');
 
 
   let rot = 0;
   let posY = 0;
 
-  fill('rgb 256,0,256');
-  ellipse( posX, posY, 5);
+  stroke('rgb 200,50,0');
+  strokeWeight(50)
+  point(0 , posY);
 
-  posX = (posY * 3)+.7;
+  posY = posY + 1
+  pop();
 }
