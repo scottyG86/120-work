5e24046d8b7fa6069bde0382eb5442f0bef64080
Scotty Graham <theboxcrab@gmail.com>
2018-09-26 21:26:55 -0600

    stuffV1.6

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -10,24 +10,25 @@ createCanvas(720,480);
 
 translate(width*.5, height*.5);
 
-    background('green');
+    background('white');
 
 }
 
 function draw()
 {
+  frameRate(120);
   let fr = frameRate();
   text( "frame rate: " + fr, 10, 20 );
   text( "posY " + posY, 10, 40 );
   text( "rot " + rot, 10, 60  );
   text( "angle " + angle, 10, 80  );
 
-  if(posY > (100 + 240))
+  if(posY > 450)
       {
-        posY = 240;
+        posY = 0;
 
-        rot = rot + 10
-        angle = radians(rot);
+
+
 
 
       }
@@ -45,10 +46,12 @@ function draw()
 
 
     rotate(angle);
-    fill('rgb(random(255),random(255),random(255))');
+    fill(color(random(255),random(255),random(255)));
     ellipse(0 , 0, 10, 30);
-    posY = posY + 1;
-    posX = posX + 1
+    posY = posY + 2;
+    posX = posX + 1;
+    rot = rot + random(100);
+    angle = radians(rot);
 
 
   pop();
