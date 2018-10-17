7082a556e8143f0b4ba93a2e1e76086258be6d04
Scotty Graham <theboxcrab@gmail.com>
2018-10-17 14:44:27 -0600

    stuffv2

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -1,22 +1,61 @@
 let particle = {};
 particle.posX = 0
 particle.posY = 0
-particle.allColorR = 0
-particle.allColorG = 0
-particle.allColorB = 0
+particle.domain = 0
+particle.colorR = 256
+particle.colorG = 0
+particle.colorB = 0
 
 function setup()
 {
   createCanvas(windowWidth,windowHeight);
   background('black')
+  frameRate(24);
 }
 function draw()
 {
-  while(posY<1024)
+  background('black')
+  translate((windowWidth*.5),0);
+  while(particle.posY<windowHeight)
   {
-     particle.posX = random(700);
-     noStroke();
-     fill(rgba(particle.allColorR,particle.allColorG,particle.allColorB,.5));
-     ellipse(posX,posY,10);
+    for(let i=0;i<2000;i++)
+    {
+      for(let i=0;i<1;i++)
+        {
+         particle.posX = random(windowWidth);
+         noStroke();
+         fill(color(particle.colorR,particle.colorG,particle.colorB));
+         ellipse(particle.posX,particle.posY,10);
+         ellipse(-particle.posX,particle.posY,10);
+        }
+      particle.posY+=.4;
+    }
+      if(particle.colorG<256&&particle.colorR==256&&particle.colorB==0)
+        {
+          particle.colorG++;
+        }
+      else if(particle.colorG==256&&particle.colorB==0&&particle.colorR>0)
+        {
+          particle.colorR--;
+        }
+      else if(particle.colorB<256&&particle.colorG==256&&particle.colorR==0)
+        {
+          particle.colorB++;
+        }
+      else if(particle.colorB==256&&particle.colorR==0&&particle.colorG>0)
+        {
+          particle.colorG--;
+        }
+      else if(particle.colorR<256&&particle.colorB==256&&particle.colorG==0)
+        {
+          particle.colorR++;
+        }
+      else if(particle.colorR==256&&particle.colorG==0&&particle.colorB>0)
+        {
+          particle.colorB--;
+        }
+
   }
+  particle.posY = 0;
+
 }
