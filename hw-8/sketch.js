f66f26d8dc90104f76f389d24564067766571c3d
Scotty Graham <theboxcrab@gmail.com>
2018-10-17 12:51:43 -0600

    stuff

---
+++ hw-8/sketch.js
@@ -0,0 +1,22 @@
+let particle = {};
+particle.posX = 0
+particle.posY = 0
+particle.allColorR = 0
+particle.allColorG = 0
+particle.allColorB = 0
+
+setup()
+{
+  createCanvas(windowWidth,windowHeight);
+  background('black')
+}
+draw()
+{
+  while(posY<1024)
+  {
+     particle.posX = random(700);
+     noStroke();
+     fill(rgba(particle.allColorR,particle.allColorG,particle.allColorB,.5));
+     ellipse(posX,posY,10);
+  }
+}
