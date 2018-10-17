cc31ee6ca82fd2abeeb2fdcfe1526b2d3ca93f55
Scotty Graham <theboxcrab@gmail.com>
2018-10-17 15:11:34 -0600

    stuffv6

--- hw-8/readme.md
+++ hw-8/readme.md
@@ -1,5 +1,13 @@
-# Empty p5 sketch.
+Scotty Graham
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+https://scottyg86.github.io/120-work/hw-8/
 
-Current p5 version -> 0.7.1
+# Summary
+  I created a color changing array of randomly placed particles
+
+  ## Details
+
+  I spent a lot of time thinking about how to make the color change in a uniform way.
+  I decided to make it generate in layers so that the entire screen would fill up with dots.
+  I used a while loop to make sure that the layers didn't generate outside of the screen.
+  I used two for loops, one to determine how many layers of a single color would be drawn and the other to determine how many particles would be drawn on each layer.
--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -9,28 +9,27 @@ particle.colorB = 0
 function setup()
 {
   createCanvas(windowWidth,windowHeight);
-
   //frameRate(24);
 }
 function draw()
 {
   background('black')
-  translate((windowWidth*.5),0);
-  while(particle.posY<windowHeight)
+  translate((windowWidth*.5),0);//sets the origin to the top center
+  while(particle.posY<windowHeight)//keeps particle inside window on y axis
   {
-    for(let i=0;i<2000;i++)
+    for(let i=0;i<2000;i++)//amount of layers drawn in a single color
     {
-      for(let i=0;i<1;i++)
+      for(let i=0;i<1;i++)//amount of particles in a layer(bigger numbers = lag)
         {
-         particle.posX = random(windowWidth);
+         particle.posX = random(windowWidth);//randomly places particle in layer
          noStroke();
          fill(color(particle.colorR,particle.colorG,particle.colorB));
-         ellipse(particle.posX,particle.posY,10);
-         ellipse(-particle.posX,particle.posY,10);
+         ellipse(particle.posX,particle.posY,10);//places particle on right side of center
+         ellipse(-particle.posX,particle.posY,10);//places an exact mirror of the particle above on the left side
         }
-      particle.posY+=.4;
+      particle.posY+=.4;//determines layer height
     }
-      if(particle.colorG<256&&particle.colorR==256&&particle.colorB==0)
+      if(particle.colorG<256&&particle.colorR==256&&particle.colorB==0)//this big group of if/else if statements is to change color by increasing a color value then decreasing the next value(Red starts at max in this case, green goes up,Red goes down,Blue goes up,Green goes down,Red goes up, Blue goes down)
         {
           particle.colorG++;
         }
@@ -56,6 +55,6 @@ function draw()
         }
 
   }
-  particle.posY = 0;
+  particle.posY = 0;//resests the Y pos to allow the while loop to start again
 
 }
