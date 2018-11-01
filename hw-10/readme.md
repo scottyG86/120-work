f13397e04c14b75b860f7f40b5029ba4cf34d930
Scotty Graham <theboxcrab@gmail.com>
2018-11-01 00:33:38 -0600

    stuff

--- hw-10/readme.md
+++ hw-10/readme.md
@@ -1,5 +1,9 @@
-# Empty p5 sketch.
+Scotty Graham
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+https://scottyg86.github.io/120-work/hw-10
 
-Current p5 version -> 0.7.1
+# Summary
+  I made a glitchy feeling square generator using two functions and an array
+
+## Details
+  Origninally I was met with some trouble making the squares fill the screen. Eventually I got it though. It kinda felt like I would patch one hole and then three more would pop up but I guess thats coding for ya. getting the squares to "glitch" was just a combination of randomly offsetting the positions and creationg gaps between layers. I thought that this week has been the most challenging by far, but I also had the most fun with it and felt the most rewarded afterwards.
--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,4 +1,4 @@
-  let squareArr = [];
+  let squareArr = [];// declares varibales
   let squareSize = 40;
   let squareY = 0;
 
@@ -10,7 +10,7 @@
             populate();
           }
         console.log("array:"+squareArr.length);
-        console.log(width/squareSize);
+        console.log(width/squareSize);// for debugging
         console.log(height/squareSize);
         console.log(squareArr);
         frameRate(24);
@@ -20,24 +20,24 @@
         background('rgb(68,34,34)');
         drawSquares();
     }
-  function drawSquares()
+  function drawSquares()//function1
   {
-    for(let i = 0; i < height/squareSize+1; i++)
+    for(let i = 0; i < height/squareSize+1; i++)//loop to make sure all Y layers are filled
       {
-        for(let i = 0; i < squareArr.length; i++)
+        for(let i = 0; i < squareArr.length; i++)// loop to make sure all X layers are filled
           {
-            fill(color(random(255),random(100),random(100)));
+            fill(color(random(255),random(100),random(100)));// gives a red primary to all the squares
             noStroke();
-            rect(squareArr[i].xPos+-random(10),squareY,squareArr[i].width,squareArr[i].height);
+            rect(squareArr[i].xPos+-random(10),squareY,squareArr[i].width,squareArr[i].height);//uses array data to draw a square
           }
-          squareY+=squareSize;
-          if(squareY>height)
+          squareY+=squareSize;// for Y layers
+          if(squareY>height)// resets Y layers so that sketch is always changing
           {
             squareY = 0+-random(10);
           }
       }
   }
-  function populate()
+  function populate()//populates array with square data and X position
     {
         squareArr.push({xPos: squareArr.length*squareSize, width: squareSize, height: squareSize});
     }
